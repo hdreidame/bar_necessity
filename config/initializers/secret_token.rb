249e7f32b7db008ddbea6b3c633d630696d3c95b
Hunter Dreidame <hunter.dreidame@gmail.com>
2011-12-14 10:31:00 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarNecessity::Application.config.secret_token = '8b85579b47a3fbb6e963f78cb1f48e04bce84d4e8a7e79067811789870febf97db602d4c87625856b76862f94ee5e52fc224ebed5ebf928af8b59a2519f74a99'
